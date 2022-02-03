37e75ec57d256232c0096a8c16a240c7b79cc4e4
BrianPulfer <brianpulfer95@gmail.com>
2022-02-03 13:20:10 +0100

    Updated ViT to feature classification token.

--- vit/vit_torch.py
+++ vit/vit_torch.py
@@ -34,23 +34,29 @@ class MyViT(nn.Module):
         # 2) Positional embedding
         # (In forward method)
 
-        # 3) Layer normalization 1
-        self.ln1 = nn.LayerNorm((self.n_patches ** 2, self.hidden_d))
+        # 3) Classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
 
-        # 4) Multi-head Self Attention (MSA)
+        # 4a) Layer normalization 1
+        self.ln1 = nn.LayerNorm((self.n_patches ** 2 + 1, self.hidden_d))
+
+        # 4b) Multi-head Self Attention (MSA) and classification token
         self.msa = MyMSA(self.hidden_d, n_heads)
 
-        # 5) Layer normalization 2
-        self.ln2 = nn.LayerNorm((self.n_patches ** 2, self.hidden_d))
+        # 5a) Layer normalization 2
+        self.ln2 = nn.LayerNorm((self.n_patches ** 2 + 1, self.hidden_d))
 
-        # 6) Encoder MLP
+        # 5b) Encoder MLP
         self.enc_mlp = nn.Sequential(
             nn.Linear(self.hidden_d, self.hidden_d),
             nn.ReLU()
         )
 
-        # 7) Classification MLP
-        self.mlp = nn.Identity(self.hidden_d * self.n_heads, out_d)  # TODO: Change
+        # 6) Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
 
     def forward(self, images):
         # Dividing images into patches
@@ -63,15 +69,22 @@ class MyViT(nn.Module):
         # Adding positional embedding
         tokens += get_positional_embeddings(self.n_patches ** 2, self.hidden_d).repeat(n, 1, 1)
 
+        # Adding classification token to the tokens
+        tokens = torch.stack([torch.vstack((self.class_token, tokens[i])) for i in range(len(tokens))])
+
         # TRANSFORMER ENCODER BEGINS ###################################
+        # NOTICE: MULTIPLE ENCODER BLOCKS CAN BE STACKED TOGETHER ######
         # Running Layer Normalization, MSA and residual connection
         out = tokens + self.msa(self.ln1(tokens))
 
-        # Running
+        # Running Layer Normalization, MLP and residual connection
         out = out + self.enc_mlp(self.ln2(out))
         # TRANSFORMER ENCODER ENDS   ###################################
 
-        return self.mlp(out.reshape(n, -1))
+        # Getting the classification token only
+        out = out[:, 0]
+
+        return self.mlp(out)
 
 
 class MyMSA(nn.Module):
@@ -129,8 +142,8 @@ def main():
     test_loader = DataLoader(test_set, shuffle=False, batch_size=16)
 
     # Defining model and training options
-    model = MyViT((1, 28, 28), n_patches=7, hidden_d=8, n_heads=2, out_d=10)
-    N_EPOCHS = 10
+    model = MyViT((1, 28, 28), n_patches=7, hidden_d=20, n_heads=2, out_d=10)
+    N_EPOCHS = 5
     LR = 0.01
 
     # Training loop
@@ -141,9 +154,9 @@ def main():
         for batch in train_loader:
             x, y = batch
             y_hat = model(x)
-            loss = criterion(y_hat, y)
+            loss = criterion(y_hat, y) / len(x)
 
-            train_loss += loss.item() / len(x)
+            train_loss += loss.item()
 
             optimizer.zero_grad()
             loss.backward()
@@ -151,13 +164,14 @@ def main():
 
         print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
 
+    # Test loop
     correct, total = 0, 0
     test_loss = 0.0
     for batch in test_loader:
         x, y = batch
         y_hat = model(x)
-        loss = criterion(y_hat, y)
-        test_loss += loss / len(x)
+        loss = criterion(y_hat, y) / len(x)
+        test_loss += loss
 
         correct += torch.sum(torch.argmax(y_hat, dim=1) == y).item()
         total += len(x)
