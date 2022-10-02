90878345c3aab2e088c2d18d715b5100d4788279
Brian Pulfer <brianpulfer95@gmail.com>
2022-10-02 14:44:23 +0200

    Normalizing Flows baseline code.

--- nf/normalizing_flows.py
+++ nf/normalizing_flows.py
@@ -3,11 +3,11 @@ Personal reimplementation of
     Density estimation using Real NVP
 (https://arxiv.org/abs/1605.08803)
 
-Usefule links:
+Useful links:
      - https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial11/NF_image_modeling.html
-     - https://github.com/VincentStimper/normalizing-flows/blob/master/normflows/flows/affine/coupling.py
 """
 
+import os
 import numpy as np
 from PIL import Image
 from tqdm.auto import tqdm
@@ -22,12 +22,13 @@ from torchvision.datasets import MNIST
 from torchvision.utils import make_grid, save_image
 from torchvision.transforms import ToTensor, Compose, Lambda
 
-from borrow import Dequantization as BorrowedDequant
+from borrow import GatedConvNet
 
 # Seeding
 np.random.seed(0)
 torch.random.manual_seed(0)
-
+torch.use_deterministic_algorithms(True)
+torch.backends.cudnn.deterministic = True
 
 def test_reversability(model, x):
     """Tests that x ≈ model.backward(model(x)) and shows images"""
@@ -53,39 +54,77 @@ def test_reversability(model, x):
         plt.title("Reconstructed image")
         plt.show()
 
-def new_convnet(k_size = 3):
-    """Returns a simple convnet"""
-    assert k_size % 2 == 1, "Kernel size must be odd"
-    
-    return nn.Sequential(
-        nn.LayerNorm((1, 28, 28)),
-        nn.Conv2d(1, 2, k_size, 1, k_size // 2),
-        nn.GELU(),
-        nn.Conv2d(2, 2, k_size, 1, k_size // 2),
-        nn.GELU(),
-        nn.Conv2d(2, 2, k_size, 1, k_size // 2),
-        nn.GELU(),
-        nn.Conv2d(2, 2, k_size, 1, k_size // 2)
-    )
 
-class MyDequantization(nn.Module):
+class SimpleCNN(nn.Module):
+    """A simple CNN architecture which will applied at each Affine Coupling step"""
+    def __init__(self, kernel_size=3, dim=32):
+        super(SimpleCNN, self).__init__()
+        self.gelu = nn.GELU()
+        self.norm_in = nn.LayerNorm((1, 28, 28))
+        self.norm_hidden = nn.LayerNorm((dim, 28, 28))
+        
+        self.old = nn.Parameter(torch.zeros(2, 28, 28))
+        self.new = nn.Parameter(torch.zeros(2, 28, 28))
+        
+        self.conv1 = nn.Conv2d(1, dim, kernel_size, 1, kernel_size//2)
+        self.conv2 = nn.Conv2d(dim, dim, kernel_size, 1, kernel_size//2)
+        self.conv3 = nn.Conv2d(dim, 2, kernel_size, 1, kernel_size//2)
+        
+        for conv in [self.conv1, self.conv2, self.conv3]:
+            conv.weight.data.zero_()
+        
+    def forward(self, x):
+        out = x
+        out = self.gelu(self.conv1(self.norm_in(out)))
+        out = self.gelu(self.conv2(self.norm_hidden(out)))
+        out = self.gelu(self.conv3(self.norm_hidden(out)))
+        return self.old * x.repeat_interleave(2, 1) + self.new * torch.tanh(out)
+
+class Dequantization(nn.Module):
     """Dequantizes the image. Dequantization is the first step for flows, as it allows to not load datapoints
     with high likelihoods and put volume on other input data as well."""
     def __init__(self):
-        super(MyDequantization, self).__init__()
-        self.sigmoid = torch.nn.Sigmoid()
+        super(Dequantization, self).__init__()
+        self.eps = 1e-5
         
-    def inv_sigmoid(self, x):
-        return - torch.log(1 / x - 1)
+        # Sigmoid and its log det
+        self.sigmoid = torch.nn.Sigmoid()
+        self.log_det_sigmoid = lambda x: self.sigmoid(x) * x
+        
+        # Inverse sigmoid and its log det
+        self.inv_sigmoid = lambda x: - torch.log((x)**-1 - 1)
+        self.log_det_inv_sigmoid = lambda x: - torch.log(x) - torch.log(1-x)
         
     def forward(self, x):
-        out = x + torch.rand_like(x).detach() / 256 
-        out = self.sigmoid(out)
-        return out, torch.log(1-torch.exp(-x)).sum(dim=[1, 2, 3])
+        # Dequantizing input (adding continuous noise in range [0, 1]) and putting in range [0, 1]
+        log_det = - np.log(256) * np.prod(x.shape[1:]) * torch.ones(len(x)).to(x.device)
+        out = (x + torch.rand_like(x).detach()) / 256
+        
+        # Making sure the input is not too close to either 0 or 1 (bounds of inverse sigmoid) --> put closer to 0.5
+        log_det += np.log(1-self.eps) * np.prod(x.shape[1:])
+        out = (1-self.eps) * out + self.eps * 0.5
+       
+        # Running the input through the inverse sigmoid function
+        log_det += self.log_det_inv_sigmoid(out).sum(dim=[1, 2, 3])
+        out = self.inv_sigmoid(out)
+        
+        return out, log_det
     
     def backward(self, x):
-        out = self.inv_sigmoid(x)
-        return out, torch.log(x**-2 / (x**-1 -1)).sum(dim=[1, 2, 3])
+        # Running through the Sigmoid function
+        log_det = self.log_det_sigmoid(x).sum(dim=[1, 2, 3])
+        out = self.sigmoid(x)
+        
+        # Undoing the weighted sum
+        log_det -= np.log(1-self.eps) * np.prod(x.shape[1:])
+        out = (out - self.eps * 0.5) / (1-self.eps)
+        
+        # Undoing the dequantization
+        log_det += np.log(256) * np.prod(x.shape[1:])
+        out *= 256
+        out = torch.floor(out).clamp(min=0, max=255)
+        
+        return out, log_det
 
 class AffineCoupling(nn.Module):
     """Affine Coupling layer. Only modifies half of the input by running the other half through some non-linear function."""
@@ -172,7 +211,7 @@ def training_loop(model, epochs, lr, wd, loader, device):
             
             # Running images forward and getting log likelihood (log_px)
             z, log_det_j = model(x)
-            log_pz = np.log(1/np.sqrt(2*np.pi)) -(z**2).sum(dim=[1,2,3]) # Because we are mapping to a normal N(0, 1)
+            log_pz = -np.log(np.sqrt(2*np.pi)) -(z**2).sum(dim=[1,2,3]) # Because we are mapping to a normal N(0, 1)
             log_px = log_pz + log_det_j
             
             # Getting the loss to be optimized (scaling with bits per dimension)
@@ -181,6 +220,7 @@ def training_loop(model, epochs, lr, wd, loader, device):
             # Optimization step
             optim.zero_grad()
             loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 1) # Clipping gradient norm
             optim.step()
             
             # Logging variable
@@ -197,12 +237,11 @@ def training_loop(model, epochs, lr, wd, loader, device):
 
 def main():
     # Program arguments
-    N_EPOCHS = 20
-    LR = 0.001
+    N_EPOCHS = 100
+    LR = 1e-4
     WD = 0.9
     
-    # Loading data (images are in range [0, 1])
-    # TODO: Set images in range [0, 255]
+    # Loading data (images are put in range [0, 255] and are copied on the channel dimension)
     transform = Compose([ToTensor(), Lambda(lambda x: 255 * x.repeat_interleave(2, 0))])
     dataset = MNIST(root='./../datasets', train=True, download=True, transform=transform)
     loader = DataLoader(dataset, batch_size=64, shuffle=True)
@@ -212,23 +251,38 @@ def main():
     
     # Creating the model
     model = Flow([
-        BorrowedDequant(),
-        AffineCoupling(new_convnet(7), modify_x2=True),
-        AffineCoupling(new_convnet(7), modify_x2=False),
-        AffineCoupling(new_convnet(5), modify_x2=True),
-        AffineCoupling(new_convnet(5), modify_x2=False),
-        # AffineCoupling(new_convnet(3), modify_x2=True),
-        # AffineCoupling(new_convnet(3), modify_x2=False),
+        Dequantization(),
+        AffineCoupling(SimpleCNN(7), modify_x2=True), #AffineCoupling(GatedConvNet(1), modify_x2=True),
+        AffineCoupling(SimpleCNN(7), modify_x2=False), #AffineCoupling(GatedConvNet(1), modify_x2=False),
+        AffineCoupling(SimpleCNN(5), modify_x2=True), #AffineCoupling(GatedConvNet(1), modify_x2=True),
+        AffineCoupling(SimpleCNN(5), modify_x2=False), #AffineCoupling(GatedConvNet(1), modify_x2=False),
+        AffineCoupling(SimpleCNN(3), modify_x2=True), #AffineCoupling(GatedConvNet(1), modify_x2=True),
+        AffineCoupling(SimpleCNN(3), modify_x2=False), #AffineCoupling(GatedConvNet(1), modify_x2=False),
+        AffineCoupling(SimpleCNN(3), modify_x2=True), #AffineCoupling(GatedConvNet(1), modify_x2=True),
+        AffineCoupling(SimpleCNN(3), modify_x2=False), #AffineCoupling(GatedConvNet(1), modify_x2=False),
     ]).to(device)
+    
+    # Showing number of trainable params
+    trainable_params = 0
+    for param in model.parameters():
+        trainable_params += np.prod(param.shape) if param.requires_grad else 0
+    print(f"The model has {trainable_params} trainable parameters.")
+    
+    # Loading pre-trained model (if any)
+    pretrained_exists = os.path.isfile("./nf_model.pt")
+    if pretrained_exists:
+        model.load_state_dict(torch.load("./nf_model.pt"))
+        print("Pre-trained model found and loaded")
     
     # Testing reversability with first image in the dataset
     test_reversability(model, dataset[0][0].unsqueeze(0).to(device))
     
-    # Training loop
-    training_loop(model, N_EPOCHS, LR, WD, loader, device)
-    
+    # Training loop (ony if model doesn't exist)
+    if not pretrained_exists:
+        training_loop(model, N_EPOCHS, LR, WD, loader, device)
+        model.load_state_dict(torch.load("./nf_model.pt"))
+        
     # Testing the trained model
-    model.load_state_dict(torch.load("./nf_model.pt"))
     model.eval()
     with torch.no_grad():
         # Mapping the normally distributed noise to new images
