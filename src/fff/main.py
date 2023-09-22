80fc71fd3ec35ab53942fcfa7c7d61a239dd3b95
Brian Pulfer <brianpulfer95@gmail.com>
2023-09-22 16:41:40 +0200

    Using hardening loss term to incentivize the network to commit at each step of the tree.

--- src/fff/main.py
+++ src/fff/main.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from pytorch_lightning.callbacks import ModelCheckpoint, TQDMProgressBar
 from pytorch_lightning.loggers import WandbLogger
-from torch.optim import Adam
+from torch.optim import SGD
 from torch.optim.lr_scheduler import LinearLR
 from torch.utils.data import DataLoader
 from torchmetrics import Accuracy
@@ -21,33 +21,48 @@ class FlattenMNIST:
 
 
 class PLWrapper(pl.LightningModule):
-    def __init__(self, model, total_iters=10):
+    def __init__(self, model, total_iters=10, hardening_weight=3.0, lr=0.2):
         super(PLWrapper, self).__init__()
         self.model = model
         self.cross_entropy = nn.CrossEntropyLoss()
         self.accuracy = Accuracy("multiclass", num_classes=10)
         self.total_iters = total_iters
+        self.hardening_weight = hardening_weight
+        self.lr = lr
 
     def forward(self, x):
         return self.model(x)
 
+    def entropy(self, x):
+        entropies = x * torch.log(x + 1e-8) + (1 - x) * torch.log(1 - x + 1e-8)
+        return -torch.mean(entropies)
+
     def configure_optimizers(self):
-        optimizer = Adam(self.model.parameters(), lr=1e-3)
+        optimizer = SGD(self.model.parameters(), lr=self.lr)
         scheduler = LinearLR(optimizer, 1, 0, total_iters=self.total_iters)
         return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.model(x)
-        loss = self.cross_entropy(y_hat, y)
+        y_hat, activations = self.model(x)
+        activations = torch.cat(activations, dim=0)
+        le = self.hardening_weight * self.entropy(activations)
+        lce = self.cross_entropy(y_hat, y)
+        loss = lce + le
         acc = self.accuracy(y_hat, y)
-        self.log("train_loss", loss)
-        self.log("train_acc", acc)
+        self.log_dict(
+            {
+                "train_loss": loss,
+                "train_loss_ce": lce,
+                "train_loss_entropy": le,
+                "train_acc": acc,
+            }
+        )
         return loss
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self.model(x)
+        y_hat, _ = self.model(x)
         loss = self.cross_entropy(y_hat, y)
         acc = self.accuracy(y_hat, y)
         self.log("test_loss", loss)
@@ -62,6 +77,12 @@ def main(args):
     # Program arguments
     batch_size = args["batch_size"]
     max_epochs = args["max_epochs"]
+    lr = args["lr"]
+    hardening_weight = args["hardening_weight"]
+    seed = args["seed"]
+
+    # Setting reproducibility
+    pl.seed_everything(seed)
 
     # Getting data
     transform = Compose([ToTensor(), FlattenMNIST()])
@@ -71,21 +92,23 @@ def main(args):
     test_loader = DataLoader(test, batch_size=batch_size, num_workers=4, shuffle=False)
 
     # Getting model
-    fff_model = nn.Sequential(
-        FFFLayer(
-            depth=3, in_dim=28 * 28, node_hidden=128, leaf_hidden=128, out_dim=100
-        ),
-        FFFLayer(depth=3, in_dim=100, node_hidden=32, leaf_hidden=32, out_dim=10),
+    fff_model = FFFLayer(
+        depth=3, in_dim=28 * 28, node_hidden=32, leaf_hidden=32, out_dim=10
     )
 
     model = PLWrapper(
         fff_model,
         total_iters=max_epochs * len(train_loader),
+        hardening_weight=hardening_weight,
+        lr=lr,
     ).train()
 
     # Training
     trainer = pl.Trainer(
-        logger=WandbLogger(name="FFF MNIST", project="Papers Re-implementations"),
+        logger=WandbLogger(
+            name="FFF MNIST",
+            project="Papers Re-implementations",
+        ),
         accelerator="auto",
         max_epochs=max_epochs,
         callbacks=[
@@ -116,8 +139,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max_epochs",
         type=int,
-        default=3,
+        default=10,
         help="Number of epochs to train the model.",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.2, help="Learning rate for the optimizer."
+    )
+    parser.add_argument(
+        "--hardening_weight",
+        type=float,
+        default=3.0,
+        help="Hardening weight for the entropy loss.",
+    )
+    parser.add_argument(
+        "--seed", type=int, default=0, help="Seed for the random number generators."
     )
     args = vars(parser.parse_args())
     print(args)
