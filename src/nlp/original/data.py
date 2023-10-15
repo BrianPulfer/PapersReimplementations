62899d223ca742b39714e1639bdb0d40685b8e3d
Brian Pulfer <brianpulfer95@gmail.com>
2023-10-15 23:47:08 +0200

    Added re-implementation of original 'attention is all you need' to translate from german to english.

---
+++ src/nlp/original/data.py
@@ -0,0 +1,90 @@
+import torch
+from datasets import load_dataset
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+
+
+class WMT14Subset(Dataset):
+    def __init__(self, subset, tokenizer, max_len=128, languages="de-en"):
+        super(WMT14Subset, self).__init__()
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.lang1, self.lang2 = languages.split("-")
+        self.subset = subset
+
+    def __len__(self):
+        return len(self.subset)
+
+    def __getitem__(self, index):
+        return self.preprocess(self.subset[index])
+
+    def preprocess(self, sample):
+        # Getting sentences
+        s1 = sample["translation"][self.lang1]
+        s2 = sample["translation"][self.lang2]
+
+        # Tokenizing sentencens
+        enc_tok = self.tokenizer(
+            s1,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_len,
+        )
+        dec_tok = self.tokenizer(
+            s2,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=self.max_len,
+        )
+
+        # Unpacking return values
+        x_enc = enc_tok["input_ids"][0]
+        x_dec = dec_tok["input_ids"][0]
+        enc_attn_mask = enc_tok["attention_mask"][0]
+        dec_attn_mask = torch.tril(torch.ones(self.max_len, self.max_len))
+        dec_attn_mask[:, x_dec == self.tokenizer.pad_token_id] = 0
+
+        return {
+            "x_enc": x_enc,
+            "x_dec": x_dec,
+            "enc_attn_mask": enc_attn_mask,
+            "dec_attn_mask": dec_attn_mask,
+            "enc_dec_attn_mask": enc_attn_mask,
+        }
+
+
+class WMT14Dataset(LightningDataModule):
+    def __init__(self, tokenizer, batch_size=32, max_len=128, languages="de-en"):
+        super(WMT14Dataset, self).__init__()
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.max_len = max_len
+        self.languages = languages
+
+    def prepare_data(self):
+        self.wmt14 = load_dataset("wmt14", self.languages)
+
+    def setup(self, stage):
+        self.train = WMT14Subset(
+            self.wmt14["train"], self.tokenizer, self.max_len, self.languages
+        )
+        self.val = WMT14Subset(
+            self.wmt14["validation"], self.tokenizer, self.max_len, self.languages
+        )
+        self.test = WMT14Subset(
+            self.wmt14["test"], self.tokenizer, self.max_len, self.languages
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.batch_size, shuffle=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch_size, shuffle=False)
+
+    def teardown(self, stage):
+        pass
---
+++ src/nlp/original/main.py
@@ -0,0 +1,144 @@
+"""
+Re-implementation of
+
+                            Attention is all you need
+                              Vaswani et al. (2017)
+                        (https://arxiv.org/abs/1706.03762)
+
+on the WMT14 dataset.
+"""
+import os
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+from transformers import AutoTokenizer
+
+from src.nlp.original.data import WMT14Dataset
+from src.nlp.original.model import EncoderDecoderModel
+
+
+def translate_sentences(file_path, model, tokenizer, max_len=128):
+    file = open(file_path, "r")
+    sentences = file.readlines()
+    file.close()
+
+    model = model.cuda()
+    dec_input = tokenizer.bos_token_id * torch.ones(1, 1)
+    dec_input = dec_input.long().cuda()
+    for i, sentence in enumerate(sentences):
+        x = tokenizer(
+            sentence,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=max_len,
+        )["input_ids"].cuda()
+        y = model.generate(x, dec_input, max_len)[0]
+        translated = tokenizer.decode(y)
+
+        print(f"\n\nSENTENCE {i+1}:")
+        print(f"\tOriginal: {sentence}\n\tTranslated: {translated}")
+
+
+def main(args):
+    # Unpacking arguments
+    enc_n_blocks = args["enc_n_blocks"]
+    enc_n_heads = args["enc_n_heads"]
+    dec_n_blocks = args["dec_n_blocks"]
+    dec_n_heads = args["dec_n_heads"]
+    hidden_dim = args["hidden_dim"]
+    dropout = args["dropout"]
+    max_len = args["max_len"]
+    batch_size = args["batch_size"]
+    max_train_steps = args["max_train_steps"]
+    lr = args["lr"]
+    weight_decay = args["weight_decay"]
+    warmup_steps = args["warmup_steps"]
+    save_dir = args["save"]
+    file_path = args["file"]
+    seed = args["seed"]
+
+    # Setting seed
+    pl.seed_everything(seed)
+
+    # Loading dataset
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    vocab_size = tokenizer.vocab_size
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({"pad_token": "<pad>"})
+        vocab_size += 1
+
+    dataset = WMT14Dataset(tokenizer, batch_size, max_len)
+
+    # Loading model
+    model = EncoderDecoderModel(
+        vocab_size,
+        max_len,
+        hidden_dim,
+        enc_n_heads,
+        enc_n_blocks,
+        dec_n_heads,
+        dec_n_blocks,
+        dropout,
+        lr,
+        weight_decay,
+        warmup_steps,
+    )
+
+    # Training
+    callbacks = [ModelCheckpoint(save_dir, monitor="val_loss", filename="best")]
+    logger = WandbLogger(project="Papers Re-implementations", name="ORIGINAL")
+    trainer = pl.Trainer(
+        devices="auto",
+        strategy="ddp",
+        max_steps=max_train_steps,
+        callbacks=callbacks,
+        logger=logger,
+    )
+    trainer.fit(model, datamodule=dataset)
+    trainer.save_checkpoint(os.path.join(save_dir, "best.ckpt"))  # TODO DELETE
+
+    # Testing
+    model = EncoderDecoderModel.load_from_checkpoint(
+        os.path.join(save_dir, "best.ckpt")
+    )
+    # trainer.test(model, datamodule=dataset) # TODO ENABLE
+
+    # Translating custom sentences
+    translate_sentences(file_path, model, tokenizer, max_len)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    # Model hyper-parameters
+    parser.add_argument("--enc_n_blocks", type=int, default=12)
+    parser.add_argument("--enc_n_heads", type=int, default=12)
+    parser.add_argument("--dec_n_blocks", type=int, default=12)
+    parser.add_argument("--dec_n_heads", type=int, default=12)
+    parser.add_argument("--hidden_dim", type=int, default=768)
+    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--max_len", type=int, default=128)
+
+    # Training parameters
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--max_train_steps", type=int, default=100_000)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--warmup_steps", type=int, default=4000)
+    parser.add_argument("--save", type=str, default="checkpoints/original")
+
+    # Testing parameters
+    parser.add_argument(
+        "--file", type=str, default="data/nlp/original/translate_sentences.txt"
+    )
+
+    # Seed
+    parser.add_argument("--seed", type=int, default=0)
+
+    args = vars(parser.parse_args())
+    print(args)
+    main(args)
